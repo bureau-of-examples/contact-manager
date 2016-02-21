f957d05402cac23082f8f135ccedee02cfbe1fd2
zhy2002 <hz141@uowmail.edu.au>
2016-02-21 12:39:49 +1100

    added icons and theming.

--- app/dist/boot.js
+++ app/dist/boot.js
@@ -1,8 +1,13 @@
 /// <reference path="_all.ts" />
 var ContactManagerApp;
 (function (ContactManagerApp) {
-    var module = angular.module('contactManagerApp', ['ngMaterial']);
+    var module = angular.module('contactManagerApp', ['ngMaterial', 'ngMdIcons']);
     module.controller('mainController', ContactManagerApp.MainController);
     module.service('userService', ContactManagerApp.UserService);
+    module.config(['$mdIconProvider', '$mdThemingProvider',
+        function ($mdIconProvider, $mdThemingProvider) {
+            $mdIconProvider.icon('menu', 'assets/svg/menu.svg', 24); //path relative to index.html
+            $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('red');
+        }]);
 })(ContactManagerApp || (ContactManagerApp = {}));
 //# sourceMappingURL=boot.js.map
--- app/src/boot.ts
+++ app/src/boot.ts
@@ -2,7 +2,13 @@
 
 module ContactManagerApp {
 
-    let module = angular.module('contactManagerApp', ['ngMaterial']);
+    let module = angular.module('contactManagerApp', ['ngMaterial', 'ngMdIcons']);
     module.controller('mainController', MainController);
     module.service('userService', UserService);
+    module.config(['$mdIconProvider', '$mdThemingProvider',
+        ($mdIconProvider: angular.material.IIconProvider, $mdThemingProvider: angular.material.IThemingProvider) => {
+            $mdIconProvider.icon('menu', 'assets/svg/menu.svg', 24); //path relative to index.html
+
+            $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('red');
+    }]);
 }
